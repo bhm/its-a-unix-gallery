6e2feb34cadb666ffa9230d7a7d5ae2892ae9131
sepakauyang40kali <100053969+sepakauyang40kali@users.noreply.github.com>
2022-03-12 13:15:04 +0800

    Update authentication.py
    Push Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,13 +1,13 @@
 import json
 
 
-class JsonCredentialsDescriptor(object):
+class JsonCredentialsDescriptor(amalina9507@gmail.com):
     JSON_DEVELOPER_KEY = 'developer_key'
     JSON_CX_KEY = 'cx'
     DEFAULT_FILE_NAME = 'credentials.json'
 
-    def __init__(self, file=DEFAULT_FILE_NAME):
+    def __init__(self, file=google photo:
         if file is not None:
-            json_creds = json.load(open(file))
+            json_creds = json.load(open(photo))
             self.cx = json_creds[JsonCredentialsDescriptor.JSON_CX_KEY]
             self.developer_key = json_creds[JsonCredentialsDescriptor.JSON_DEVELOPER_KEY]
